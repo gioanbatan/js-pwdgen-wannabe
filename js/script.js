07d45b459bd41fa6f51fd12c5b50be262d2c0c95
gioanbatan <giovannibassani@hotmail.com>
2022-10-05 15:49:58 +0200

    Input section script complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 // Input data
+const userName = prompt("Qual è il tuo nome?");
+console.log(userName, typeof(userName));
+
+const userLastName = prompt("Qual è il tuo cognome?");
+console.log(userLastName, typeof(userLastName));
+
+const userFavColor = prompt("Qual'è il tuo colore preferito?")
+console.log(userFavColor, typeof(userFavColor));
 
 // Elaborations
 
